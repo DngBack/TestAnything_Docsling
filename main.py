33fdeee9ab4036a8bb1624450dc7bead8d7b47b3
bachdx-1327 <duong.xuan.bach@sun-asterisk.com>
2024-12-12 15:48:28 +0000

    Add test chunking

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
 # try simple code with one paper
 from docling.document_converter import DocumentConverter
+from docling.chunking import HybridChunker
+from transformers import AutoTokenizer
+
+import os 
+from dotenv import load_dotenv, dotenv_values
+
+import tiktoken
+
+from openai import OpenAI
+
+load_dotenv()
+
+EMBED_MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
+OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+MAX_TOKENS = 512
 
 # Try with local data
-source = "2024-nissan-leaf-owner-manual.pdf"  # document per local path or URL
+source = "data/2-109_QUY TRÌNH CẬP NHẬT QUY CHẾ TÀI CHÍNH.xlsx"  # document per local path or URL
 converter = DocumentConverter()
 result = converter.convert(source)
+
 print(result.document.export_to_markdown())
 # output: ## Docling Technical Report [...]"
+
+# test hybrid chunking
+# DOC_SOURCE = "data/CLIP.pdf"
+
+# # Get docs
+# doc = DocumentConverter().convert(source=DOC_SOURCE).document
+
+# # Set tokenizer
+# # Initialize the OpenAI API client
+# client = OpenAI(api_key=OPENAI_KEY)
+
+# tokenizer = AutoTokenizer.from_pretrained(EMBED_MODEL_ID)
+
+
+# chunker = HybridChunker(
+#     tokenizer=tokenizer,  # can also just pass model name instead of tokenizer instance
+#     max_tokens=MAX_TOKENS,  # optional, by default derived from `tokenizer`
+#     # merge_peers=True,  # optional, defaults to True
+# )
+# chunk_iter = chunker.chunk(dl_doc=doc)
+# chunks = list(chunk_iter)
+
+# for i, chunk in enumerate(chunks):
+#     print(f"=== {i} ===")
+#     txt_tokens = len(tokenizer.tokenize(chunk.text, max_length=None))
+#     print(f"chunk.text ({txt_tokens} tokens):\n{repr(chunk.text)}")
+
+#     ser_txt = chunker.serialize(chunk=chunk)
+#     ser_tokens = len(tokenizer.tokenize(ser_txt, max_length=None))
+#     print(f"chunker.serialize(chunk) ({ser_tokens} tokens):\n{repr(ser_txt)}")
+
+#     print()
